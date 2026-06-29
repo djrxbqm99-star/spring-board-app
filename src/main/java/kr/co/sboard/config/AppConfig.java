eeb8c83c5a659e67711afdfa12ab5c4d78a7c48a
chhak0503 <chhak0503@gmail.com>
2026-06-29 17:10:16 +0900

    commit 0.0.1

--- src/main/java/kr/co/sboard/config/AppConfig.java
+++ src/main/java/kr/co/sboard/config/AppConfig.java
@@ -1,7 +1,6 @@
 package kr.co.sboard.config;
 
 import kr.co.sboard.dto.AppInfoDTO;
-import kr.co.sboard.interceptor.AppInfoInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,17 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
-    private AppInfoDTO appInfo;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AppInfoInterceptor(appInfo));
-    }
-
-    @Bean
-    public AppInfoDTO appInfo(){
-        appInfo = new AppInfoDTO();
-        return appInfo;
-    }
 
 }
